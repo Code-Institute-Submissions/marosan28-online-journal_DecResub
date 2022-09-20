dfd2a000f407ec36ffe1861778c0395ba586e69b
marosan28 <danijela.marosan36@gmail.com>
2022-09-20 21:29:33 +0000

    add csrf funcionality for django 4

--- online_journal/settings.py
+++ online_journal/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-marosan28-onlinejournal-urlpykfzfi6.ws-eu64.gitpod.io']
