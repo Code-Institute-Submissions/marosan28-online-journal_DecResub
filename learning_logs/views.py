cbea9580e4efa0a92b70325510eb4c3c3cfd26e7
marosan28 <danijela.marosan36@gmail.com>
2022-09-21 10:50:15 +0000

    Fix topic views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,9 +8,8 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
-def topic(request, topic_id):
-    """Show a topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries, }
-    return render(request, 'learning_logs/topic.html', context)
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
