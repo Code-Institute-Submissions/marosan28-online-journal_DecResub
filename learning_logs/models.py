75f3f014e7f9274c5534bb08048dc0cfa3d17c7f
marosan28 <danijela.marosan36@gmail.com>
2022-09-21 09:57:26 +0000

    Add index page and a template

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,14 +12,10 @@ class Topic(models.Model):
 
 
 class Entry(models.Model):
-    """Information about a topic"""
+    """Learning log entries for a topic."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        verbose_name_plural = 'entries'
-
     def __str__(self):
-        """Return a string of a model"""
-        return f"{self.text[:50]}..."
+        return self.text[:50]
