cbc8d9d2d067e832607e31be167dc4e4b62404dd
marosan28 <danijela.marosan36@gmail.com>
2022-09-21 11:11:11 +0000

    Add individual topic pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,6 +9,11 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    # Page that shows all topics.
-    path('topics/', views.topics, name= 'topics'),
+
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+
+    # Specific section for a certain topic
+    path('topics/<int:topic_id>', views.topics, name='topic'),
+
 ]
