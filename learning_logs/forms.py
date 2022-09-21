a5a9e7e3c25aadc40a9b002c6f4351345a6ceaa4
marosan28 <danijela.marosan36@gmail.com>
2022-09-21 20:07:48 +0000

    Fix entry form

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -2,13 +2,17 @@ from django import forms
 
 from .models import Topic, Entry
 
+
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
-        labels = {'text' : ''}
+        labels = {'text': ''}
+
+
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['text']
-        labels = {'text': forms.Textarea(attrs={'cols:60'})}
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 60})}
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -23,6 +23,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+
 def new_topic(request):
     """Introduce a new topic."""
     if request.method == 'POST':
@@ -37,3 +38,22 @@ def new_topic(request):
     # Show a blank or incomplete form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """New entry"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data
+        form = EntryForm()
+    else:
+        # Data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
