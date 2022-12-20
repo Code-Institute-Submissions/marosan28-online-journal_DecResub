b704f32505fc4836b9ded612220e18fc21bf8888
marosan28 <danijela.marosan36@gmail.com>
2022-12-20 18:47:02 +0000

    add subscribers model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import SubscribedUsers
 
-# Register your models here.
+class SubscribedUsersAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'created_date')
+
+admin.site.register(SubscribedUsers, SubscribedUsersAdmin)
