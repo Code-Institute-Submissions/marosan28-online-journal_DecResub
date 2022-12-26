5031d992144c4a0aceab68bb409bde27fa80788b
marosan28 <danijela.marosan36@gmail.com>
2022-12-26 18:13:08 +0000

    setup development environment

--- online_journal/settings.py
+++ online_journal/settings.py
@@ -15,6 +15,9 @@ import dj_database_url
 from django.contrib.messages import constants as messages
 import mimetypes
 
+
+development = os.environ.get('DEVELOPMENT', False)
+
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
         messages.INFO: 'alert-info',
@@ -37,7 +40,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'kingolina??25')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = development
 
 # Application definition
 
@@ -96,19 +99,26 @@ WSGI_APPLICATION = 'online_journal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+
+if development:
+    ALLOWED_HOSTS = ['localhost']
 
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
